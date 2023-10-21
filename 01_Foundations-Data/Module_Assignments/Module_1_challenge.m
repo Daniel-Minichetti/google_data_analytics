d347c0473347e0402680956ea91ed77222e4befa
Daniel Minichetti <98673196+Daniel-Minichetti@users.noreply.github.com>
2023-10-20 22:14:20 -0700

    Create Module_1_challenge.m

---
+++ 01_Foundations-Data/Module_Assignments/Module_1_challenge.m
@@ -0,0 +1,150 @@
+### 1.
+
+Question 1
+
+Data analysis is the various elements that interact with one another in order to provide, manage, store, organize, analyze, and share data.
+
+1 / 1 point
+
+******\[ ]True**
+
+******\[x]False**
+
+Correct
+
+
+### 2.
+
+Question 2
+
+Fill in the blank: Data ecosystems are made up of elements that \_\_\_\_\_ with each other. This makes it possible for them to produce, manage, store, organize, analyze, and share data.
+
+1 / 1 point
+
+******\[ ]gain insights**
+
+******\[ ]unite**
+
+******\[x]interact**
+
+******\[ ]problem-solve**
+
+Correct
+
+
+### 3.
+
+Question 3
+
+Fill in the blank: Using data to create new ways of understanding and modeling the unknown is known as \_\_\_\_\_.
+
+1 / 1 point
+
+******\[ ]data design**
+
+******\[ ]data analysis**
+
+******\[x]data science**
+
+******\[ ]data engineering**
+
+Correct
+
+
+### 4.
+
+Question 4
+
+Fill in the blank: In data science, \_\_\_\_\_\_\_\_ is when a data analyst uses their unique past experiences to understand the story the data is telling.
+
+1 / 1 point
+
+******\[ ]awareness**
+
+******\[ ]rational thought**
+
+******\[ ]personal opinion**
+
+******\[x]gut instinct**
+
+Correct
+
+
+### 5.
+
+Question 5
+
+If you are using data-driven decision-making, what action steps would you take? Select all that apply.
+
+1 / 1 point
+
+******\[x]Drawing conclusions from your analysis**
+
+Correct
+
+******\[ ]Surveying customers about results, conclusions, and recommendations**
+
+******\[x]Sharing your results with subject matter experts**
+
+Correct
+
+******\[x]Gathering and analyzing data**
+
+Correct
+
+
+### 6.
+
+Question 6
+
+To get the most out of data-driven decision-making, it’s important to include insights from people very familiar with the business problem. What are these people called?
+
+1 / 1 point
+
+******\[ ]Customers**
+
+******\[ ]Stakeholders**
+
+******\[ ]Competitors**
+
+******\[x]Subject-matter experts**
+
+Correct
+
+
+### 7.
+
+Question 7
+
+As a data analyst, you finish analyzing the latest marketing data. If you are following the data-driven decision making process, what should you do next?
+
+1 / 1 point
+
+******\[ ]Create a model based on the results of the analysis**
+
+******\[ ]Survey customers about results, conclusions, and recommendations**
+
+******\[x]Share the results with subject-matter experts from the marketing team for their input**
+
+******\[ ]Archive the datasets in order to keep them secure**
+
+Correct
+
+
+### 8.
+
+Question 8
+
+When citing an article you found in a discussion forum, you should always do what?
+
+1 / 1 point
+
+******\[ ]Check the article for typos or grammatical errors.**
+
+******\[ ]Take credit for creating the article.**
+
+******\[ ]Include your email address for people to send questions or comments.**
+
+******\[x]Give credit to the original author.**
+
+Correct
